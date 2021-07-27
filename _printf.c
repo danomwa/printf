e1c204301465bb3e266159ac1e0330ef8a146af8
danomwa <84831872+danomwa@users.noreply.github.com>
2021-07-27 19:35:56 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,48 +1,45 @@
 #include "holberton.h"
+
 /**
- * _printf - replication of some of the features from C function printf()
- * @format: character string of directives, flags, modifiers, & specifiers
- * Description: This function uses the variable arguments functionality and is
- * supposed to resemble printf().  Please review the README for more
- * information on how it works.
- * Return: number of characters printed
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifier
+ *
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-	va_list args_list;
-	inventory_t *inv;
-	void (*temp_func)(inventory_t *);
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	if (!format)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(args_list, format);
-	inv = build_inventory(&args_list, format);
-
-	while (inv && format[inv->i] && !inv->error)
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		inv->c0 = format[inv->i];
-		if (inv->c0 != '%')
-			write_buffer(inv);
-		else
+		if (*p == '%')
 		{
-			parse_specifiers(inv);
-			temp_func = match_specifier(inv);
-			if (temp_func)
-				temp_func(inv);
-			else if (inv->c1)
+			p++;
+			if (*p == '%')
 			{
-				if (inv->flag)
-					inv->flag = 0;
-				write_buffer(inv);
+				count += _putchar('%');
+				continue;
 			}
-			else
-			{
-				if (inv->space)
-					inv->buffer[--(inv->buf_index)] = '\0';
-				inv->error = 1;
-			}
-		}
-		inv->i++;
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-	return (end_func(inv));
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
